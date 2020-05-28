7c66cd7297e5280cdda11848f86f05f9ad2ebd80
Akhamammir <it@paseusted.com.mx>
2020-05-28 17:02:26 -0500

    handler for kittys

--- Models/mongooseModels.js
+++ Models/mongooseModels.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var kittySchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
   Name:{First:String, Last:String, Last2:String}, Birth:String, Gender:String,
   Curp:String, RFC:String, DateH:String, Pos:String, Phone:String, Emergency:String,
   Academic: String, Pwd: String, Usr:String
@@ -15,8 +15,12 @@ var ClientSchema = new mongoose.Schema({
   regFiscal:[], isn:String, rtp:String, cedular:String, ish:String,
   succList:[], contactList:[], repList:[]
 });
+var kittySchema = new mongoose.Schema({
+  API_KEY: String, active: Boolean
+})
 module.exports = {
     Person : mongoose.model('UserList', new mongoose.Schema({}), 'Users' ),
-    User: mongoose.model('User', kittySchema, 'Users'),
-    Client: mongoose.model('Client', ClientSchema, 'Clients')
+    User: mongoose.model('User', UserSchema, 'Users'),
+    Client: mongoose.model('Client', ClientSchema, 'Clients'),
+    Kitty: mongoose.model('Kitty', kittySchema, 'APIs')
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var Models = require('./../Models/mongooseModels');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/', function (req, res, next) {
+  var query = Models.Kitty.findOne({ API_KEY: req.body.API });
+  query.exec(function (err, kitty) {
+    if (err) {
+      console.log(err)
+      return res.send(500, { error: err });
+    }
+    console.log(kitty, 'person, line 10');
+    if (kitty.toJSON().Active) {
+      res.send({ 'server': "http://35.236.92.7:3000" })
+    } else { res.render('index', { title: 'Express' }); }
+  });
+});
 module.exports = router;
